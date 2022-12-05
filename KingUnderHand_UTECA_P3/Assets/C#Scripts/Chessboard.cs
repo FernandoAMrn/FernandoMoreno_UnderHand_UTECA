1d7b88c27a08bf4a2b4a6b3fa053bb2a9954adf9
Fernando Moreno Arroyo <fernandoAMrn@gmail.com>
2022-12-04 19:08:56 -0600

    Tablero creado con offset
    Haciendo un offset para crear el tableto donde deberia aparecer.

--- KingUnderHand_UTECA_P3/Assets/C#Scripts/Chessboard.cs
+++ KingUnderHand_UTECA_P3/Assets/C#Scripts/Chessboard.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.EventSystems;
 public class Chessboard : MonoBehaviour
 {
     [Header("Art Stuff")]
     [SerializeField] private Material tileMaterial;
     [SerializeField] private Material hoverMaterial;
+    [SerializeField] private float tileSize = 1.0f;
+    [SerializeField] private float yOffset = 0.2f;
+    [SerializeField] private Vector2 boardCenter = Vector2.zero;
 
     // LOGIC 
     private const int TILE_COUNT_X = 4;
@@ -15,6 +18,8 @@ public class Chessboard : MonoBehaviour
     private GameObject[,] tiles;
     private Camera currentCamera;
     private Vector2Int currentHover;
+
+    private Vector2 bounds;
     private void Awake()
     {
         GenerateAllTiles(1, (float)1.5, TILE_COUNT_X, TILE_COUNT_Y);
@@ -66,6 +71,9 @@ public class Chessboard : MonoBehaviour
     //Crear Tablero
     private void GenerateAllTiles(float tileSizeX, float tileSizeY, int tileCountX, int tileCountY) //Creacion de todos los recuadros
     {
+        yOffset += transform.position.y;
+        bounds = new Vector2((tileCountX / 2) * tileSizeX,(tileCountX / 2) * tileSizeY) + boardCenter;
+
         tiles = new GameObject[tileCountX, tileCountY];
         for (int x = 0; x < tileCountX; x++)
         {
@@ -85,10 +93,10 @@ public class Chessboard : MonoBehaviour
         tileObject.AddComponent<MeshRenderer>().material = tileMaterial;
 
         Vector3[] vertices = new Vector3[4];
-        vertices[0] = new Vector2(x * tileSizeX, y * tileSizeY);
-        vertices[1] = new Vector2(x * tileSizeX, (y + 1) * tileSizeY);
-        vertices[2] = new Vector2((x + 1) * tileSizeX, y * tileSizeY);
-        vertices[3] = new Vector2((x + 1) * tileSizeX, (y + 1) * tileSizeY);
+        vertices[0] = new Vector2(x * tileSizeX, y * tileSizeY)-bounds;
+        vertices[1] = new Vector2(x * tileSizeX, (y + 1) * tileSizeY)-bounds;
+        vertices[2] = new Vector2((x + 1) * tileSizeX, y * tileSizeY)-bounds;
+        vertices[3] = new Vector2((x + 1) * tileSizeX, (y + 1) * tileSizeY)-bounds;
 
         int[] tris = new int[] { 0, 1, 2, 1, 3, 2 };
         mesh.vertices = vertices;
@@ -98,6 +106,7 @@ public class Chessboard : MonoBehaviour
 
         tileObject.layer = LayerMask.NameToLayer("Tile");
         tileObject.AddComponent<BoxCollider>();
+
 
         return tileObject;
     }
