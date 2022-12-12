3806d7cb4db3f3c795db7dee6bf3a7487580451a
Fernando Moreno Arroyo <fernandoAMrn@gmail.com>
2022-12-12 08:16:14 -0600

    Sistema de turnos
    Sistema de turnos funcional

--- KingUnderHand_UTECA_P3/Assets/C#Scripts/Draggable.cs
+++ KingUnderHand_UTECA_P3/Assets/C#Scripts/Draggable.cs
@@ -5,39 +5,78 @@ using UnityEngine.EventSystems;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    
     public Transform parentToReturnTo = null;
-   public void OnBeginDrag(PointerEventData eventData)
+    public bool isBlackTurn;
+
+    private void Start()
+    {
+        isBlackTurn = true;
+    }
+
+    public void changeTurns()
+    {
+        isBlackTurn = !isBlackTurn;
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
     {
         //Debug.Log("OnBeginDrag");
-        parentToReturnTo = this.transform.parent;
-        this.transform.SetParent(this.transform.parent.parent);
+        if (isBlackTurn == true)
+        {
+            parentToReturnTo = this.transform.parent;
+            this.transform.SetParent(this.transform.parent.parent);
+            GetComponent<CanvasGroup>().blocksRaycasts = false;
+        }
+        else
+        {
+            return;
+        }
+        
 
-        GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         //Debug.Log("OnDrag");
-        this.transform.position = eventData.position;
+        if (isBlackTurn == true)
+        {
+            this.transform.position = eventData.position;
+        }
+        else
+        {
+            return;
+        }
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        //Debug.Log("OnEndDrag");
-        this.transform.SetParent(parentToReturnTo);
-        GetComponent<CanvasGroup>().blocksRaycasts = true;
 
+        //Debug.Log("OnEndDrag");
+
+        if (isBlackTurn == true)
+        {
+            this.transform.SetParent(parentToReturnTo);
+            GetComponent<CanvasGroup>().blocksRaycasts = true;
+        }
+        else
+        {
+            return;
+        }
         
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
 
-        if (other.tag == "WhiteCard")
+        if (other.CompareTag("WhiteCard"))
         {
             Debug.Log("Coliding with white card");
             this.transform.SetParent(parentToReturnTo);
             GetComponent<CanvasGroup>().blocksRaycasts = true;
         }
     }
+
+
 }
--- KingUnderHand_UTECA_P3/Assets/C#Scripts/Draggable_W.cs
+++ KingUnderHand_UTECA_P3/Assets/C#Scripts/Draggable_W.cs
@@ -5,27 +5,65 @@ using UnityEngine.EventSystems;
 
 public class Draggable_W : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    
     public Transform parentToReturnTo = null;
+    public bool isWhiteTurn;
+
+    private void start()
+    {
+        isWhiteTurn = false;
+    }
+
+    public void changeTurn()
+    {
+        isWhiteTurn = !isWhiteTurn;
+    }
+
     public void OnBeginDrag(PointerEventData eventData)
     {
-        //Debug.Log("OnBeginDrag");
-        parentToReturnTo = this.transform.parent;
-        this.transform.SetParent(this.transform.parent.parent);
+        if (isWhiteTurn == true)
+        {
+            //Debug.Log("OnBeginDrag");
+            parentToReturnTo = this.transform.parent;
+            this.transform.SetParent(this.transform.parent.parent);
+            GetComponent<CanvasGroup>().blocksRaycasts = false;
+        }
+        else
+        {
+            return;
+        }
+        
 
-        GetComponent<CanvasGroup>().blocksRaycasts = false;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        //Debug.Log("OnDrag");
-        this.transform.position = eventData.position;
+        if (isWhiteTurn == true)
+        {
+            //Debug.Log("OnDrag");
+            this.transform.position = eventData.position;
+        }
+        else
+        {
+            return;
+        }
+        
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         //Debug.Log("OnEndDrag");
-        this.transform.SetParent(parentToReturnTo);
-        GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        if (isWhiteTurn == true)
+        {
+            this.transform.SetParent(parentToReturnTo);
+            GetComponent<CanvasGroup>().blocksRaycasts = true;
+        }
+        else
+        {
+            return;
+        }
+        
 
 
     }
@@ -33,7 +71,7 @@ public class Draggable_W : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndD
     private void OnTriggerEnter2D(Collider2D other)
     {
         
-        if (other.tag == "BlackCard")
+        if (other.CompareTag("BlackCard"))
         {
             Debug.Log("Coliding with black card");
             this.transform.SetParent(parentToReturnTo);
